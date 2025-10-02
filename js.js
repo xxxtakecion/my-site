ec1e3faacb0901bea384ba87871fbbcbfe46d242
xxxtakecion <1jinyoung8846@gmail.com>
2025-10-02 12:18:29 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+            $(function(){
+                $('.popup').hide();
+
+                $('#tesx>a').click(function(){
+                    $('.popup').fadeIn();
+                });
+
+
+                $('.but2').click(function(){
+                    $('.popup').fadeOut();
+                });
+            });
